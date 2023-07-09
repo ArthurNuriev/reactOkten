ae2bc1c217798245656b38a6cb50a5c24b5c3ab8
ArthurNuriev <artnrv.10@gmail.com>
2023-07-10 01:30:56 +0300

    hw02 added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ import {useState} from "react";
 function App() {
     const [userId, setUserId] = useState(null)
     return (
-        <div className="App">
+        <div className="Appp">
 
             <Posts/>
             <Flights/>
