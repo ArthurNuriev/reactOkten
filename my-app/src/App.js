3b8b7354f39ad99da55513f4b681d20734c27b31
ArthurNuriev <artnrv.10@gmail.com>
2023-07-13 13:37:02 +0300

    hw 04 added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {UserComponent} from "./components/UserComponent/UserComponent";
+import {PostComponent} from "./components/PostComponent/PostComponent";
+import {CreateCar} from "./components/CarComponent/CreateCar";
+import {DeleteCar} from "./components/CarComponent/DeleteCar";
+import {UpdateCar} from "./components/CarComponent/UpdateCar";
+
+
+
 
 function App() {
 
     return (
         <div className="App">
-            App
+            <UserComponent/>
+            <PostComponent/>
+            <CreateCar/>
+            <DeleteCar/>
+            <UpdateCar/>
+
+
         </div>
     );
 }
