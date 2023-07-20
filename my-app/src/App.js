2050194aa67c6a98473212b0703d59d01165b546
ArthurNuriev <artnrv.10@gmail.com>
2023-07-20 23:46:12 +0300

    hw06 fix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,9 +5,9 @@ import {CarsComponents} from "./CarsComponents/CarsComponents";
 function App() {
 
     return (
- CarsForm
+
         <div className="App">
-            <CarsComponents/>
+
         </div>
 
     );
